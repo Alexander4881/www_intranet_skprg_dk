6c84703fed62d26c333361da39f1182207010f01
iZeQure <iZeQure@users.noreply.github.com>
2019-04-08 14:50:09 +0200

    Add project files.

---
+++ www_intranet_skprg_dk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace www_intranet_skprg_dk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
